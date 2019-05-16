010c372521d39bc5338afa8e3ebe8a216ec4fa4f
liangtao <84568403@qq.com>
2019-05-16 15:37:54 +0800

    模板

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,11 @@
 
 module.exports = {
     // 基本路径
-    publicPath: '/127.0.0.1:3000',
+    publicPath: '/',
     // 输出文件目录
     outputDir: 'dist',
     // webpack-dev-server 相关配置
     devServer: {
-        open: process.platform === 'darwin',
         host: '0.0.0.0',
         port: 8080,
         https: false,
